7b9357d5797034146dabff29e0ce3e879fb342b7
ppadron5 <35806348+ppadron5@users.noreply.github.com>
2018-02-19 09:41:20 -0600

    Began Yahtzee

---
+++ Yahtzee/yahtzeeObject1.js
@@ -0,0 +1,50 @@
+yahtzee = {
+  'turnsRemaining' : 13,
+  'throwsRemainingInTurn' : 3,
+  'player' : {
+    'name' : 'Bill',
+    'avatar' : 'https://',
+  }
+  'dice' : [
+    {
+      'sideUp' : 4,
+      'saved' : false,
+    },
+    {
+      'sideUp' : 1,
+      'saved' : true,
+    },
+    {
+      'sideUp' : 6,
+      'saved' : false,
+    },
+    {
+      'sideUp' : 2,
+      'saved' : true,
+    },
+    {
+      'sideUp' : 5,
+      'saved' : false,
+    }
+  ]
+  'scoreCard' = [
+    {
+      'title' : 'Full House',
+      'top' : false,
+      'displaySequence' : 9,
+      'scoreRecorded' : true,
+      'score' : 25,
+      'scoreCondition' : 'notSureYet',
+      'scoreMath' : 'fixedScoreOf25'
+    }
+    {
+      'title' : 'Ones',
+      'top' : true,
+      'displaySequence' : 1,
+      'scoreRecorded' : false,
+      'score' : 1,
+      'scoreCondition' : 'notSureYet',
+      'scoreMath' : 'sumOfOnes'
+    }
+  ]
+ }
