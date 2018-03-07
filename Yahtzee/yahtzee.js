9c7040d71bb3de32229cb8d93bc55828d641a0c2
ppadron5 <35806348+ppadron5@users.noreply.github.com>
2018-03-07 09:38:04 -0600

    added modal

--- Yahtzee/yahtzee.js
+++ Yahtzee/yahtzee.js
@@ -1,8 +1,26 @@
 function setup() {
+  loadModal();
   loadPlayerInfo();
   loadDice();
   loadScorecard();
 }
+
+function loadModal() {
+  modal = document.getElementsByClassName('modal-wrapper')[0];
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  name = document.getElementById('nameInput').value;
+  if (name.length > 0) {
+    document.getElementById('name').innerHTML = name;
+    CheckedImage = document.querySelector('input[name=avatar][checked]').value;
+    document.getElementsByClassName('avatar').src = CheckedImage;
+    modal = document.getElementsByClassName('modal-wrapper')[0];
+    modal.style.display = "none";
+  }
+}
+
 
 function loadPlayerInfo() {
   document.getElementById('playerName').innerHTML = yahtzee.player.name;
@@ -114,7 +132,7 @@ function calculateScores() {
       } else {
         scoreCardRow.score = 0;
       }
-    } 
+    }
   });
   loadScorecard();
 }
